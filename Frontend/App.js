a5ec005ddf0c3f5342258af1d72beac5c32fea07
Ramya B L <67257046+ram-ctrl@users.noreply.github.com>
2025-02-07 23:22:43 +0530

    Create App.js

---
+++ Frontend/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
