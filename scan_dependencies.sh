f0baf4704d6183eab417412c31e4504bbc5de16d
Ramya B L <67257046+ram-ctrl@users.noreply.github.com>
2025-02-08 11:57:27 +0530

    Update scan_dependencies.sh

--- scan_dependencies.sh
+++ scan_dependencies.sh
@@ -9,3 +9,19 @@ fi
 echo "Scanning dependencies..."
 trivy fs --format json --output kbom.json .
 
+
+#!/bin/bash
+
+echo "Generating KBOM using Syft..."
+syft backend -o json > backend-kbom.json
+syft frontend -o json > frontend-kbom.json
+
+echo "Scanning for vulnerabilities using Trivy..."
+trivy fs --format json --output backend-vulnerabilities.json backend
+trivy fs --format json --output frontend-vulnerabilities.json frontend
+
+echo "Merging KBOM & Vulnerabilities..."
+
+python3 merge_kbom.py
+
+
